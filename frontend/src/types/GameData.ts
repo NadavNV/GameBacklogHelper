996b245b8f285d2e0a31ef91afb5c71df2c97fe9
NadavNV <nadav.nevo@gmail.com>
2025-09-26 10:38:55 +0300

    Replaced hardcoded values with types

--- frontend/src/types/GameData.ts
+++ frontend/src/types/GameData.ts
@@ -1,9 +1,11 @@
+import type { LengthKey } from "src/constants/lengths";
 import type { PlatformName } from "src/constants/platforms";
+import type { StatusKey } from "src/constants/statuses";
 
 export interface GameData {
   title: string;
   platform: PlatformName;
-  status: "backlog" | "playing" | "finished" | "abandoned";
-  length: "short" | "medium" | "long" | "notAvailable";
+  status: StatusKey;
+  length: LengthKey;
   metacriticScore?: number;
 }
